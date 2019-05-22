73a5d5328d5afab192b61611b134ddf9b4a8b523
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-05-22 14:38:15 +0400

    Comment changes

--- lib/use_cases.ex
+++ lib/use_cases.ex
@@ -3,4 +3,4 @@ end
 
 # Generating same table schema like for activity_logging in different application.
 #     Instead of defining in different application and using Projects Repo for transacting.
-#     Create a macro with all method to perfomr on that table with that inject Repo as argument. 
+#     Create a macro with all method to perfomr on that table with that inject Repo as argument.
